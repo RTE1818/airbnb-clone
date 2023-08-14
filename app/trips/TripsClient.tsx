eaa4acda4e6d01ac61fe8463b57a316768e49ebd
Robert Eaton <eatonrobert97@gmail.com>
2023-08-13 22:30:01 -0500

    deployable project 7

--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -43,16 +43,7 @@ const TripsClient: React.FC<TripsClientProps> = ({ reservations, currentUser }) 
       />
       <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         {reservations.map((reservation) => (
-          <ListingCard 
-            key={reservation.id}
-            data={reservation.listingId}
-            reservation={reservation}
-            actionId={reservation.id}
-            onAction={onCancel}
-            disabled={deletingId === reservation.id}
-            actionLabel="Cancel Reservation"
-            currentUser={currentUser}
-          />
+          <div key={reservation.id}></div>
         ))}
       </div>
     </Container>
