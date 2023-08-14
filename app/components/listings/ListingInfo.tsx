aa7de5adce6cb9e5b03e136cf450a1c3926f2677
Robert Eaton <eatonrobert97@gmail.com>
2023-08-13 21:50:51 -0500

    deployable project take 2

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -12,7 +12,7 @@ const Map = dynamic(() => import('../Map'), {
 }) 
 
 interface ListingInfoProps {
-  user: airbnbUser;
+  user?: airbnbUser;
   description: string;
   guestCount: number;
   roomCount: number;
@@ -34,8 +34,8 @@ const ListingInfo: React.FC<ListingInfoProps> = ({user, description, guestCount,
     <div className="col-span-4 flex flex-col gap-8">
       <div className="flex flex-col gap-2">
         <div className="text-xl font-semibold flex flex-row items-center gap-2">
-          <div>Hosted by {user?.name}</div>
-          <Avatar src={user?.image}/>
+          <div>Hosted by Rob</div>
+          <Avatar src="/images/avatarimg.jpg"/>
         </div>
 
         <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -104,7 +104,6 @@ const ListingClient: React.FC<ListingClientProps> = ({ reservations = [], listin
           />
           <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
             <ListingInfo 
-              user={listing.user}
               category={category}
               description={listing.description}
               roomCount={listing.roomCount}
